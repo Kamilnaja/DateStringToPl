22ae72535fdf44b60755e71e080f56dc80e6c3f4
kamilnaja <kamilnaja@gmail.com>
2018-10-28 21:12:31 +0100

    added parsing months

--- polishDateFormat/PolishDateFormat.js
+++ polishDateFormat/PolishDateFormat.js
@@ -9,6 +9,7 @@ module.exports = class PolishDateFormat {
         this.dateInPolishFormat = "";
         this.polishDays = ["poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"];
         this.englishDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+        this.englishMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         this.polishMonths = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"];
     }
 
@@ -24,6 +25,11 @@ module.exports = class PolishDateFormat {
         let englishDay = givenDate.substring(0, givenDate.indexOf(" "));
         let englishDayIndex = this.englishDays.indexOf(englishDay);
         this.dateInPolishFormat = this.polishDays[englishDayIndex];
+
+        let newDateString = givenDate.substring(givenDate.indexOf(" ") + 1);
+        let monthString = newDateString.substring(0, newDateString.indexOf(" "));
+        let englishMonthIndex = this.englishMonths.indexOf(monthString);
+        this.monthInPolishFormat = this.polishMonths[englishMonthIndex];
         return this.getDateInPolishFormat();
     }
 };
--- test/index.js
+++ test/index.js
@@ -1,10 +1,12 @@
-const PolishDateFormat = require('./node_modules/polishDateFormat');
+const PolishDateFormat = require('./../polishDateFormat');
 let dateParser = new PolishDateFormat();
 //tests
-let date1 = new Date(1970, 1, 1);
-let date2 = new Date(2003, 3, 3);
+let date1 = new Date(1970, 2, 1);
+let date2 = new Date(2003, 11, 3);
+
+console.log(date2.toDateString());
 
 console.assert(
     dateParser.parseDate(date1.toDateString()) === "niedziela");
-console.assert(
-    dateParser.parseDate(date2.toDateString()) === 'czwartek');
+// console.assert(
+//     dateParser.parseDate(date2.toDateString()) === 'czwartek');
