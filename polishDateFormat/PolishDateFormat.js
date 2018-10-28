bfb440fc560bc9a83d7681854b8ec569d36be417
kamilnaja <kamilnaja@gmail.com>
2018-10-28 22:03:37 +0100

    code refactor

--- polishDateFormat/PolishDateFormat.js
+++ polishDateFormat/PolishDateFormat.js
@@ -1,7 +1,6 @@
 'use strict';
 
-/** this application, should get date and returns date in polish languate format.
- * @param givenDate - date to parse
+/** this application, should get date and returns date (week day and month) in polish languate format.
  */
 
 module.exports = class PolishDateFormat {
@@ -11,25 +10,31 @@ module.exports = class PolishDateFormat {
         this.englishDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
         this.englishMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         this.polishMonths = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"];
+        this.dayInPolishFormat = "";
+        this.monthInPolishFormat = "";
     }
 
     getDateInPolishFormat() {
-        return this.dateInPolishFormat;
-    }
-
-    setDateInPolishFormat(date) {
-        this.dateInPolishFormat = date;
+        return [this.dayInPolishFormat, this.monthInPolishFormat].join(" ")
     }
 
     parseDate(givenDate) {
-        let englishDay = givenDate.substring(0, givenDate.indexOf(" "));
-        let englishDayIndex = this.englishDays.indexOf(englishDay);
-        this.dateInPolishFormat = this.polishDays[englishDayIndex];
+        this.parseWeekDays(givenDate);
+        this.parseMonths(givenDate);
 
-        let newDateString = givenDate.substring(givenDate.indexOf(" ") + 1);
-        let monthString = newDateString.substring(0, newDateString.indexOf(" "));
-        let englishMonthIndex = this.englishMonths.indexOf(monthString);
-        this.monthInPolishFormat = this.polishMonths[englishMonthIndex];
         return this.getDateInPolishFormat();
+    }
+
+    parseMonths(givenDate) {
+        let dateEntityString = givenDate.substring(givenDate.indexOf(" ") + 1);
+        let englishDateFragment = dateEntityString.substring(0, dateEntityString.indexOf(" "));
+        let englishDateIndex = this.englishMonths.indexOf(englishDateFragment);
+        this.monthInPolishFormat = this.polishMonths[englishDateIndex];
+    }
+
+    parseWeekDays(givenDate) {
+        let dateEntityString = givenDate.substring(0, givenDate.indexOf(" "));
+        let englishDateIndex = this.englishDays.indexOf(dateEntityString);
+        this.dayInPolishFormat = this.polishDays[englishDateIndex];
     }
 };
--- test/index.js
+++ test/index.js
@@ -4,9 +4,10 @@ let dateParser = new PolishDateFormat();
 let date1 = new Date(1970, 2, 1);
 let date2 = new Date(2003, 11, 3);
 
+console.log(date1.toDateString());
 console.log(date2.toDateString());
 
 console.assert(
-    dateParser.parseDate(date1.toDateString()) === "niedziela");
-// console.assert(
-//     dateParser.parseDate(date2.toDateString()) === 'czwartek');
+    dateParser.parseDate(date1.toDateString()) === "niedziela marzec");
+console.assert(
+    dateParser.parseDate(date2.toDateString()) === 'środa grudzień');
